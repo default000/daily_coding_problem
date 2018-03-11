9fdbd61bdffece211b60bdf0a320f7c8844157b9
default000 <jon.piva@gmail.com>
2018-03-10 17:51:09 -0800

    adding questions

---
+++ 001.rb
@@ -0,0 +1,5 @@
+# This problem was asked by Google.
+
+# Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. 
+
+# For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3].
---
+++ 004.rb
@@ -0,0 +1,5 @@
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. You can modify the input array in-place.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
